cb01c9128e6e07479487e8c363c7aef93c3c4e91
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-02-02 22:46:44 +0100

    Cleanup and refactor of template
    Commit will:
    * remove About.vue
    * move Home.vue to components from views
    * refactor router.ts

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Home extends Vue {
+  private message: string = 'Welcome home!';
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+import Home from '@/components/Home.vue';
+import { RouterOptions, RouteConfig, RouterMode } from 'vue-router/types/router';
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-  ],
-});
+const routerOptions: RouterOptions = {};
+const routes: RouteConfig[] = [
+   {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+];
+const mode: RouterMode = 'history';
+
+routerOptions.routes = routes;
+routerOptions.mode = mode;
+
+export default new Router(routerOptions);
